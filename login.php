5e2ac57983b09c44db93564601feaae22d101b50
unTrick <kim_patrick001@yahoo.com>
2016-08-27 18:26:27 +0800

    edit

--- login.php
+++ login.php
@@ -60,94 +60,23 @@
 
 	input[type="password"] {
 		border-radius: 8px;
-		height:40px; 
+		height:50px; 
 		clear:both;
 		text-align: center;
 		width: 400px;
 		margin-bottom: 25px;
-	}
-
-	input[type="submit"]{
-		width: 180px;
-		height: 40px;
-		margin: 20px;
-		border-radius: 8px;
-		background-color: rgb(0,176,78);
-		border-color: transparent;
-		color: 
-
-	}
-
-	p {
-		margin-bottom: 25px;
-		width: 100%;
-		color: red;
-	}
-
-@media only screen and (min-device-width : 768px) and (max-device-width : 1024px) and (orientation : landscape) {
-		body {
-		width: 100%;
-		font-family: arial;
-	}
-
-	.main-container{
-		margin-top: 300px;
-		width: 100%;
-		clear: both;
-		text-align: center;
-
-	}
-
-	img {
-		width: 300px;
-		margin-bottom: 25px;
-	}
-
-	.form-contain {
-		width: 60%;
-		border-radius: 30px;
-		background-color: rgba(1,1,1,.75);
-		text-align: center;
-		clear: both;
-		padding-top: 80px;
-		padding-bottom: 60px;
-	}
-
-	label {
-		width: 100%;
 		font-size: 24px;
-		color: white;
-		float: left;
-		text-align: center;
-		margin: 5px;
-	}
-
-	input[type="text"]{
-		border-radius: 8px;
-		height:40px; 
-		clear:both;
-		width: 400px;
-		text-align: center;
-		margin-bottom: 25px;
-	}
-
-	input[type="password"] {
-		border-radius: 8px;
-		height:40px; 
-		clear:both;
-		text-align: center;
-		width: 400px;
-		margin-bottom: 25px;
 	}
 
 	input[type="submit"]{
 		width: 180px;
-		height: 40px;
+		height: 50px;
 		margin: 20px;
 		border-radius: 8px;
 		background-color: rgb(0,176,78);
 		border-color: transparent;
-		color: 
+		color: white;
+		font-size: 24px;
 
 	}
 
@@ -157,8 +86,6 @@
 		color: red;
 	}
 
-
-}
 
 @media only screen and (min-width: 540px) and (min-height: 960px) and (orientation: portrait){
 	body {
@@ -209,21 +136,23 @@
 
 	input[type="password"] {
 		border-radius: 8px;
-		height:40px; 
+		height:50px; 
 		clear:both;
 		text-align: center;
 		width: 400px;
 		margin-bottom: 25px;
+		font-size: 24px;
 	}
 
 	input[type="submit"]{
 		width: 180px;
-		height: 40px;
+		height: 50px;
 		margin: 20px;
 		border-radius: 8px;
 		background-color: rgb(0,176,78);
 		border-color: transparent;
-		color: 
+		color: white;
+		font-size: 24px;
 
 	}
 
@@ -253,14 +182,15 @@
 	<div class="main-container">
 		<img src="img/logomuna.jpg">
 		<div class="form-contain">
-			<form method="POST" action="login.php">
+			<form method="POST" action="">
 				<label>Ranger Name:</label>
 				<input type="text" name="rname">
 				<label>Password:</label>
 				<input type="password" name="pass">
 				<p><?php
 				echo $warning; ?></p>
-				<input type="submit" value="LOGIN" name="login"><input type="submit" value="REGISTER" name="reg">
+				<input type="submit" value="LOGIN" name="login">
+				<input type="submit" value="REGISTER" name="reg" action="register.php">
 
 			</form>
 
@@ -281,18 +211,20 @@
 if(isset($_POST['login'])){
 
 
-$name = $_POST['rname'];
-$pass = $_POST['pass'];
+	$name = $_POST['rname'];
+	$pass = $_POST['pass'];
 
-if(($name == "patrick") && ($pass == "pass")){
-	$warning = "Success!";
-}
+	if(($name == "patrick") && ($pass == "pass")){
+		$warning = "Success!";
+	}
 
-else {
-	$warning = "* Wrong Password of Username";
-}
+	else {
+		$warning = "* Wrong Password of Username";
+	}
 
 
+}else if(isset($_POST['reg'])){
+	echo "<script>window.location.href='register.php'</script>";
 }
 
 ?>
