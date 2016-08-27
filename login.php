6c2545eec7110133a6d57419db8f8996a5cc24e5
unTrick <kim_patrick001@yahoo.com>
2016-08-27 17:37:00 +0800

    commit ni pat

--- login.php
+++ login.php
@@ -11,6 +11,79 @@
 	margin: 0px auto;
 	padding: 0px auto;
 }
+@media only screen and (min-width: 900px) and (min-height: 960px) and (orientation: portrait){
+	body {
+		width: 100%;
+		font-family: arial;
+	}
+
+	.main-container{
+		margin-top: 300px;
+		width: 100%;
+		clear: both;
+		text-align: center;
+
+	}
+
+	img {
+		width: 300px;
+		margin-bottom: 25px;
+	}
+
+	.form-contain {
+		width: 60%;
+		border-radius: 30px;
+		background-color: rgba(1,1,1,.75);
+		text-align: center;
+		clear: both;
+		padding-top: 80px;
+		padding-bottom: 60px;
+	}
+
+	label {
+		width: 100%;
+		font-size: 24px;
+		color: white;
+		float: left;
+		text-align: center;
+		margin: 5px;
+	}
+
+	input[type="text"]{
+		border-radius: 8px;
+		height:40px; 
+		clear:both;
+		width: 400px;
+		text-align: center;
+		margin-bottom: 25px;
+	}
+
+	input[type="password"] {
+		border-radius: 8px;
+		height:40px; 
+		clear:both;
+		text-align: center;
+		width: 400px;
+		margin-bottom: 25px;
+	}
+
+	input[type="submit"]{
+		width: 180px;
+		height: 40px;
+		margin: 20px;
+		border-radius: 8px;
+		background-color: rgb(0,176,78);
+		border-color: transparent;
+		color: white;
+
+	}
+
+	p {
+		margin-bottom: 25px;
+		width: 100%;
+		color: red;
+	}
+
 
 @media only screen and (min-width: 540px) and (min-height: 960px) and (orientation: portrait){
 	body {
